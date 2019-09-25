0b7360915c56835086a9523fd63f5b1d2a54aa5f
denisenes <55022112+denisenes@users.noreply.github.com>
2019-09-25 17:51:03 +0700

    Update sqr_roots.hs

--- sqr_roots.hs
+++ sqr_roots.hs
@@ -1,4 +1,4 @@
-resfn a b c | a /= 0 = (((-b) + sqrt(b*b - 4*a*c))/(2*a), ((-b) - sqrt(b*b - 4*a*c))/(2*a))
-            | a == 0 && b /= 0 = ((-c) / b, (-c) / b)
-            | a == 0 && b == 0 && c /= 0 = error "A and B can't be null"
-            | a == 0 && b == 0 && c == 0 = error " Infinity"
+resfn 0 0 0 = error " Infinity"
+resfn 0 0 c = error "A and B can't be null"
+resfn 0 b c = ((-c) / b, (-c) / b)
+resfn a b c = (((-b) + disc)/(2*a), ((-b) - disc)/(2*a)) where disc = sqrt(b*b - 4*a*c)
